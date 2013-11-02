381782faa5104af4843bc25f4d03b0d0ebd3229a
George Opritescu <ssscripting@gmail.com>
2013-11-02 22:56:15 +0200

    provisioner

--- provisioner.sh
+++ provisioner.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
+
+set -x
+set -e
+
+sudo apt-get install -y ruby1.9.1-dev git-core
+
 pushd `pwd`
 if rvm --version 2>/dev/null; then
-  gem install soloist
+  gem install soloist -v 1.0.0.pre --no-ri --no-rdoc
 else
-  sudo gem install soloist
+  sudo gem install soloist -v 1.0.0.pre --no-ri --no-rdoc
 fi
 
 mkdir -p ~/cookbooks; cd ~/cookbooks
