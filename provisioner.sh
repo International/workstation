fcc90c47c2ec344f90acefa2b37b6d10fbf8dbcc
George Opritescu <ssscripting@gmail.com>
2013-11-02 23:07:14 +0200

    Mirror chef-repo structure

--- provisioner.sh
+++ provisioner.sh
@@ -6,9 +6,12 @@ set -e
 sudo apt-get install -y ruby1.9.1-dev git-core
 
 pushd `pwd`
+
 if rvm --version 2>/dev/null; then
   gem install soloist -v 1.0.0.pre --no-ri --no-rdoc
+  gem install librarian-chef --no-ri --no-rdoc
 else
+  sudo gem install librarian-chef --no-ri --no-rdoc
   sudo gem install soloist -v 1.0.0.pre --no-ri --no-rdoc
 fi
 
