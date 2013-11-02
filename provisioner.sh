966f7476c80edca7c902f2461a682f4df4eb45b3
George Opritescu <ssscripting@gmail.com>
2013-11-02 23:21:41 +0200

    Adding gemfile

--- provisioner.sh
+++ provisioner.sh
@@ -9,9 +9,9 @@ pushd `pwd`
 
 if rvm --version 2>/dev/null; then
   gem install soloist -v 1.0.0.pre --no-ri --no-rdoc
-  gem install librarian-chef --no-ri --no-rdoc
+  gem install librarian-chef -v 0.0.2 --no-ri --no-rdoc
 else
-  sudo gem install librarian-chef --no-ri --no-rdoc
+  sudo gem install librarian-chef -v 0.0.2 --no-ri --no-rdoc
   sudo gem install soloist -v 1.0.0.pre --no-ri --no-rdoc
 fi
 
