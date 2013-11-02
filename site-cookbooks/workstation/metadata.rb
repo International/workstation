1136819faa400f25c134e402b059cfc93b1f7f44
George Opritescu <ssscripting@gmail.com>
2013-11-03 00:54:00 +0200

    track cookbooks + site-cookbooks

---
+++ site-cookbooks/workstation/metadata.rb
@@ -0,0 +1,7 @@
+name             'workstation'
+maintainer       'George Opritescu'
+maintainer_email 'ssscripting@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures workstation'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
