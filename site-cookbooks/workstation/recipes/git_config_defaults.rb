656819761548f4b723ccc3572905cb15ceec1f2b
George Opritescu <ssscripting@gmail.com>
2013-11-03 04:24:56 +0200

    prompt

--- site-cookbooks/workstation/recipes/git_config_defaults.rb
+++ site-cookbooks/workstation/recipes/git_config_defaults.rb
@@ -1,6 +1,8 @@
 ["color.ui true", 
  "user.name '#{node["solo_git_name"]}'", 
- "user.email '#{node["solo_git_email"]}'"].each do |git_cmd|
+ "user.email '#{node["solo_git_email"]}'",
+ "difftool.prompt false"
+].each do |git_cmd|
    execute "command #{git_cmd}" do
      command "git config --global #{git_cmd}"
      user node["solo_username"]
