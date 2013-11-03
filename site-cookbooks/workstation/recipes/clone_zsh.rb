bf6c11ade0fec4a29d13bb74cca83be4af5b3aaa
George Opritescu <ssscripting@gmail.com>
2013-11-03 20:46:15 +0200

    setup zsh

--- site-cookbooks/workstation/recipes/clone_zsh.rb
+++ site-cookbooks/workstation/recipes/clone_zsh.rb
@@ -1,5 +1,11 @@
 git "#{node["solo_home"]}/.oh-my-zsh" do
   repository "https://github.com/robbyrussell/oh-my-zsh.git"
   action "sync"
-  owner node["solo_username"]
+  user node["solo_username"]
+  notifies :run, "bash[setup_zshrc]"
+end
+
+bash "setup_zshrc" do
+  command "cp -f #{node["solo_home"]}/.oh-my-zsh/templates/zshrc.zsh-template #{node["solo_home"]}/.zshrc"
+  action :nothing
 end
