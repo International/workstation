a9c1964ae054b2041b3689a19c6f28a6aa3b845f
George Opritescu <ssscripting@gmail.com>
2013-11-03 21:41:53 +0200

    changed to execute

--- site-cookbooks/workstation/recipes/clone_zsh.rb
+++ site-cookbooks/workstation/recipes/clone_zsh.rb
@@ -2,11 +2,11 @@ git "#{node["solo_home"]}/.oh-my-zsh" do
   repository "https://github.com/robbyrussell/oh-my-zsh.git"
   action "sync"
   user node["solo_username"]
-  notifies :run, "bash[setup_zshrc]"
+  notifies :run, "execute[setup_zshrc]"
 end
 
-bash "cp #{node["solo_home"]}/.oh-my-zsh/templates/zshrc.zsh-template #{node["solo_home"]}/.zshrc" do
-  name "setup_zshrc"
+execute "setup_zshrc" do
+  command "cp #{node["solo_home"]}/.oh-my-zsh/templates/zshrc.zsh-template #{node["solo_home"]}/.zshrc"
   action :nothing
   user node["solo_username"]
 end
