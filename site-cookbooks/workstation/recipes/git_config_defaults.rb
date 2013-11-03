a8ea5552a790b03303cc4420fa90d0f4371d8cef
George Opritescu <ssscripting@gmail.com>
2013-11-03 04:24:20 +0200

    loop fix

--- site-cookbooks/workstation/recipes/git_config_defaults.rb
+++ site-cookbooks/workstation/recipes/git_config_defaults.rb
@@ -1,6 +1,6 @@
 ["color.ui true", 
  "user.name '#{node["solo_git_name"]}'", 
- "user.email '#{node["solo_git_email"]}"].each do |git_cmd|
+ "user.email '#{node["solo_git_email"]}'"].each do |git_cmd|
    execute "command #{git_cmd}" do
      command "git config --global #{git_cmd}"
      user node["solo_username"]
