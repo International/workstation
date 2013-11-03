ee5bc2c87eae58e5f76600b35ac0f634eb5b0efa
George Opritescu <ssscripting@gmail.com>
2013-11-03 21:35:24 +0200

    name

--- site-cookbooks/workstation/recipes/clone_zsh.rb
+++ site-cookbooks/workstation/recipes/clone_zsh.rb
@@ -5,8 +5,8 @@ git "#{node["solo_home"]}/.oh-my-zsh" do
   notifies :run, "bash[setup_zshrc]"
 end
 
-bash "setup_zshrc" do
-  command "cp -f #{node["solo_home"]}/.oh-my-zsh/templates/zshrc.zsh-template #{node["solo_home"]}/.zshrc"
+bash "cp #{node["solo_home"]}/.oh-my-zsh/templates/zshrc.zsh-template #{node["solo_home"]}/.zshrc" do
+  name "setup_zshrc"
   action :nothing
   user node["solo_username"]
 end
