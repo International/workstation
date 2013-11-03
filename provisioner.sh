599ee63ac609e5574362e89fcf49ce1cd4f1096e
George Opritescu <ssscripting@gmail.com>
2013-11-03 03:23:49 +0200

    chef

--- provisioner.sh
+++ provisioner.sh
@@ -20,10 +20,10 @@ else
 
   if rvm --version 2>/dev/null; then
     gem install soloist -v $soloist_version --no-ri --no-rdoc
-    gem install librarian-chef -v 0.0.2 --no-ri --no-rdoc
+    # gem install librarian-chef -v 0.0.2 --no-ri --no-rdoc
   else
-    sudo gem install librarian-chef -v $soloist_version --no-ri --no-rdoc
-    sudo gem install soloist -v 1.0.0.pre --no-ri --no-rdoc
+    sudo gem install soloist -v $soloist_version --no-ri --no-rdoc
+    # sudo gem install librarian-chef -v 0.0.2 --no-ri --no-rdoc
   fi
 fi
 
