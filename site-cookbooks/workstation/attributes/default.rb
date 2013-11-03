4e356d4aadcf8b3b3ce652c09085ef8115522726
George Opritescu <ssscripting@gmail.com>
2013-11-03 02:25:29 +0200

    owner

--- site-cookbooks/workstation/attributes/default.rb
+++ site-cookbooks/workstation/attributes/default.rb
@@ -1,1 +1,1 @@
-node.default["workstation"]["git_config_defaults"]["git_config_owner_user"] = "vagrant"
+node.default["workstation"]["git_config_defaults"]["git_config_owner_user"] = ENV["OWNER_USER"] || "vagrant"
