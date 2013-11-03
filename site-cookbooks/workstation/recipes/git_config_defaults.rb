82e7ec0952bd2598791c02a12346d0d48b42000c
George Opritescu <ssscripting@gmail.com>
2013-11-03 03:54:13 +0200

    bash

--- site-cookbooks/workstation/recipes/git_config_defaults.rb
+++ site-cookbooks/workstation/recipes/git_config_defaults.rb
@@ -1,9 +1,9 @@
-execute "su -u #{node["solo_username"]} git config --global color.ui true" do
+execute "bash -c 'git config --global color.ui true'" do
   user  node["solo_username"]
   group node["solo_username"]
 end
 
-execute "su -u #{node["solo_username"]} git config --global user.name #{node["solo_git_name"]} user.email #{node["solo_git_email"]}" do
+execute "bash -c 'git config --global user.name #{node["solo_git_name"]} user.email #{node["solo_git_email"]}'" do
   user  node["solo_username"]
   group node["solo_username"]
 end
