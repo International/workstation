d78c14a4b1fa11dafc90e9006986263b4b331152
George Opritescu <ssscripting@gmail.com>
2013-11-03 04:16:54 +0200

    set username and email also

--- site-cookbooks/workstation/recipes/git_config_defaults.rb
+++ site-cookbooks/workstation/recipes/git_config_defaults.rb
@@ -2,3 +2,8 @@ execute "setting color.ui true" do
   command "git config --global color.ui true"
   user  node["solo_username"]
 end
+
+execute "configure username and email" do
+  command "git config --global user.name #{node["solo_git_name"]} user.email #{node["solo_git_email"]}"
+  user node["solo_username"]
+end
