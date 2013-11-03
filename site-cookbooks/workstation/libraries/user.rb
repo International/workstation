a506cd7432c91fa8d31f7d5c434fb1f91327ead2
George Opritescu <ssscripting@gmail.com>
2013-11-03 02:27:31 +0200

    new override

--- site-cookbooks/workstation/libraries/user.rb
+++ site-cookbooks/workstation/libraries/user.rb
@@ -1,1 +1,1 @@
-WS_USER=ENV["SUDO_USER"] || ENV["USER"]
+WS_USER=node["workstation"]["git_config_defaults"]["git_config_owner_user"] || ENV["SUDO_USER"] || ENV["USER"]
--- site-cookbooks/workstation/recipes/git_config_defaults.rb
+++ site-cookbooks/workstation/recipes/git_config_defaults.rb
@@ -1,2 +1,6 @@
 execute "echo #{WS_USER}"
 execute "echo #{node["workstation"]["git_config_defaults"]["git_config_owner_user"]}"
+
+# execute "git config --global color.ui true" do
+#   user 
+# end
