36f3e04c5764403c6827fb9446a339f5ffe50d07
George Opritescu <ssscripting@gmail.com>
2013-11-03 04:23:25 +0200

    loop

--- site-cookbooks/workstation/recipes/git_config_defaults.rb
+++ site-cookbooks/workstation/recipes/git_config_defaults.rb
@@ -1,14 +1,8 @@
-execute "setting color.ui true" do
-  command "git config --global color.ui true"
-  user  node["solo_username"]
-end
-
-execute "configure username" do
-  command "git config --global user.name '#{node["solo_git_name"]}'"
-  user node["solo_username"]
-end
-
-execute "configure email" do
-  command "git config --global user.email #{node["solo_git_email"]}"
-  user node["solo_username"]
+["color.ui true", 
+ "user.name '#{node["solo_git_name"]}'", 
+ "user.email '#{node["solo_git_email"]}"].each do |git_cmd|
+   execute "command #{git_cmd}" do
+     command "git config --global #{git_cmd}"
+     user node["solo_username"]
+   end
 end
